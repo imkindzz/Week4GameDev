07e0912cf9a1b4546fd491728a40df142ee5f3fd
imkindzz <142460540+imkindzz@users.noreply.github.com>
2023-08-29 15:25:36 +1000

    90% complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     public new int i = 3;
     public GameObject ObjName;
+    public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
