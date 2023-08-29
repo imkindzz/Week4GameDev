be03006a279395a51acd930a3f466d2b66fa4704
imkindzz <142460540+imkindzz@users.noreply.github.com>
2023-08-29 15:49:59 +1000

    100% Complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -7,16 +7,30 @@ public class PrintAndHide : MonoBehaviour
     public new int i = 3;
     public GameObject ObjName;
     public Renderer rend;
+    public int random;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        random = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        rend = GetComponent<Renderer>();
         i++;
         Debug.Log(ObjName.name +":" + i);
+
+        if (ObjName.tag == ("Red") && i > 100)
+        {
+            ObjName.SetActive(false);
+        }
+
+        if (ObjName.tag == ("Blue") && i >= random)
+        {
+            rend.enabled = false;
+        }
     }
 }
